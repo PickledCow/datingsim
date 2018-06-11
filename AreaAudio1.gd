09716f5b17c91511e42ad753e998931fda9b6e4a
Unknown <joon.suh1@education.nsw.gov.au>
2018-06-11 22:15:49 +1000

    Area Audio

---
+++ AreaAudio1.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+onready var player = get_node("../Player")
+onready var audio = get_node("AudioStreamPlayer2D")
+var touching = false
+var audioPlaying = false
+
+func _ready():
+	pass
+
+func _process(delta):
+	if overlaps_body(player) and not touching:
+		touching = true
+		if not audioPlaying:
+			audio.volume_db = 0
+			audio.play()
+			audioPlaying = true
+		else:
+			audio.volume_db = 0
+	elif not overlaps_body(player) and touching:
+		touching = false
+		audio.volume_db = -80
